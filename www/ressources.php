d563a16a29e71d43e2175e1daa1b746e167b6179
Thibaut <thibaut.villeneuve@epsi.fr>
2019-03-18 12:00:03 +0100

    function for minio

---
+++ www/ressources.php
@@ -0,0 +1,32 @@
+<?php
+
+require '../vendor/autoload.php';
+
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1',
+    'endpoint' => '0.0.0.0:9000',
+    'use_path_style_endpoint' => true,
+    'credentials' => [
+        'key'    => 'minio',
+        'secret' => 'minio123',
+    ],
+]);
+
+
+// Send a PutObject request and get the result object.
+$insert = $s3->putObject([
+    'Bucket' => 'testbucket',
+    'Key'    => 'testkey',
+    'Body'   => 'Hello from Minio!!'
+]);
+
+$retrive = $s3->getObject([
+    'Bucket' => 'testbucket',
+    'Key'    => 'testkey',
+    'SaveAs' => 'testkey_local'
+]);
+
+echo $retrive['Body'];
+
+?>
